edc9614bcf41c60c7470e06e3e90ace9b66164ed
zhangdongyang <zhangdongyang@xiaomi.com>
2023-10-09 19:43:41 +0800

    init project

---
+++ src/test/java/com/zdy/java/springboot/demo/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package com.zdy.java.springboot.demo;
+
+import com.zdy.java.springboot.demo.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+}
